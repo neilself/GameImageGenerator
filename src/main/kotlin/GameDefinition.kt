e34702577602db4ca03a0d9ed5c171d1a1d15159
Neil Self <neilself@gmail.com>
2021-02-06 17:37:11 +0100

    Clean up commented-out code

--- src/main/kotlin/GameDefinition.kt
+++ src/main/kotlin/GameDefinition.kt
@@ -1,10 +1,5 @@
 package com.cicero.cardgame
 
-//class GameDefinition {
-//  lateinit var game_title: String
-//  lateinit var game_card_collections: List<Map<String, String>>
-//}
-
 data class GameEntityCollection(val data_filename: String, val layout_filename: String, val output_filename_prefix:
 String)
 
